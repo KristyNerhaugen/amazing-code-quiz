6f3c1704e7cd25bf06cb6e3197ca024e6b082322
kristynerhaugen <kristynerhaugen@gmail.com>
2022-05-25 12:38:39 -0500

    starting html outline and starting pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Click to start quiz
+
+// arrays of objects for each question
+
+// function to start the quiz and timer
+
+// tell user if answer is correct or wrong, if wrong subtract time
+
+// allow for input of initials with score
+
+// store high scores
